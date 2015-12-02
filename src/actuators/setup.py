07b0d1e7403264b93fe35c37836ee8020987b7af
smaria <sms4g13@soton.ac.uk>
2015-12-02 20:23:24 +0000

    fill in example setup.py

---
+++ src/actuators/setup.py
@@ -0,0 +1,13 @@
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+## http://docs.ros.org/api/catkin/html/howto/format2/installing_python.html
+## pdf download of the page in sources folder:
+## docs-ros_installing_python.pdf
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=['actuators'],
+    package_dir={'': 'src'})
+
+setup(**setup_args)
