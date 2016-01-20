e525384dd1f04600da3b12bc56c7a323b211ca3a
tsaoyu <tsaoyu@tsaoyu.com>
2016-01-20 20:14:34 +0000

    include a PID controller library and updated control loop in scripts

--- src/lowlevel_control/src/lowlevel_control/pid_data.py
+++ src/lowlevel_control/src/lowlevel_control/pid_data.py
@@ -1,9 +1,17 @@
 import rospy
 
+
 class PID_Data:
     def __init__(self):
         self.goal_heading = 0.
-
+        self.heading = 0.
 
     def update_goal_heading(self, msg):
+        """
+        Read in new data for PID controller
+        :param msg:
+        """
         self.goal_heading = msg.data
+
+    def update_heading(self, msg):
+        self.heading = msg.data
