177ed2818d42df77c43263782636c5d0851bcc89
smaria <sms4g13@soton.ac.uk>
2016-01-27 21:26:52 +0000

    remove laylines, use side headings to determin if in irons

--- src/highlevel_control/src/highlevel_control/heading_planning.py
+++ src/highlevel_control/src/highlevel_control/heading_planning.py
@@ -1,15 +1,14 @@
 import rospy
-#import LatLon
+import LatLon as ll
 
 class HeadingPlan:
     def __init__(self):
         self.wind_direction = 0.
-        self.waypoint = {'lat':50.742810 , 'lon':-1.014469} #somewhere in the solent
-        self.position = {'lat':50.8, 'lon':-1.02}
+        self.waypoint = ll.LatLon(50.742810, 1.014469) #somewhere in the solent
+        self.position = ll.LatLon(50.8,1.02)
+        self.dead_zone = 90 #assuming symmetric dead zone, total angle range of dead zone
         self.heading = 0
         self.wp_heading = self.heading
-        self.port_layline = 0
-        self.stb_layline = 0
         self.side_heading = 0
         self.alternate_heading = 0
         self.sailing_state = 'normal'  # sailing state can be 'normal','tack_to_port_tack' or  'tack_to_stbd_tack'
@@ -26,12 +25,10 @@ class HeadingPlan:
         self.heading = msg.data
 
     def update_waypoint(self, msg):
-        self.waypoint['lat'] = msg.latitude
-        self.waypoint['long'] = msg.longitude
+        self.waypoint = ll.LatLon(msg.latitude, msg.longitude)
 
     def update_position(self, msg):
-        self.position['lat'] = msg.latitude
-        self.position['long'] = msg.longitude
+        self.position = ll.LatLon(msg.latitude, msg.longitude)
 
 
     ##################
@@ -39,30 +36,52 @@ class HeadingPlan:
     # calculate various directions
     #
     ##################
-    def calculate_laylines(self):
-        # assume laylines are +/- 45 degrees upwind
-        # (total 'in irons' area: 90 degrees)
-        self.port_layline = (self.wind_direction - (180 - 45)) % 360
-        self.stbd_layline = (self.wind_direction - (180 + 45)) % 360
-
     def calculate_wp_heading(self):
-        pass
+        self.wp_heading = self.position.heading_initial(self.waypoint)
 
-    def calculate_side_heading(self):
-        pass
+    def calculate_side_headings(self):
+        # assume dead zones are +/- 45 degrees upwind
+        # (total 'in irons' area: 90 degrees)
+        
+        headingA = angleSum(self.wind_direction, self.dead_zone/2.)
+        headingB = angleSum(self.wind_direction, -self.dead_zone/2.)
+        if angleAbsDistance(self.heading, headingA) < angleAbsDistance(self.heading, headingB):
+            self.side_heading = headingA
+            self.alternate_heading = headingB
+        else:
+            self.side_heading = headingB
+            self.alternate_heading = headingA
 
-    def calculate_alternate_heading(self):
-        pass
+
+
 
     ##################
     #
     # Checks for state determination
     #
     ##################
-    def check_outside_laylines(self):
-        return True
+    def check_outside_dead_zone(self):
+        # if the distance to the alternate heading is larger than the dead zone angle
+        # the boat must be outside the laylines
+        if angleAbsDistance(self.heading, self.alternate_heading) > self.dead_zone:
+            return True
+        else:
+            return False
 
     def determine_sailing_state(self):
         pass
 
+################
+#
+# General utility functions
+#
+################
+
+def angleSum(a,b):
+    return (a+b)%360
+
+def angleAbsDistance(a,b):
+    distanceA = abs((a - b)%360)
+    distanceB = abs((a - b - 360)%360)
+    return min(distanceA, distanceB)
 
