2c8bdcbddb6ebd2b249f1aa59ada54ef5fd1df09
tsaoyu <tsaoyu@tsaoyu.com>
2016-01-27 19:14:20 +0000

    Updated rudder PID control node

--- src/lowlevel_control/src/lowlevel_control/pid_data.py
+++ src/lowlevel_control/src/lowlevel_control/pid_data.py
@@ -3,15 +3,29 @@ import rospy
 
 class PID_Data:
     def __init__(self):
+        self.tack_rudder = 0.
+        self.update_sailing_state = ''
         self.goal_heading = 0.
         self.heading = 0.
 
     def update_goal_heading(self, msg):
         """
-        Read in new data for PID controller
+        Update goal heading data from higher level controller for PID controller
         :param msg:
         """
         self.goal_heading = msg.data
 
+    def update_sailing_state(self, msg):
+
+        self.update_sailing_state = msg.data
+
     def update_heading(self, msg):
+        """
+        Get continuous update of current heading from sensors
+        :param msg:
+
+        """
         self.heading = msg.data
+
+    def update_tack_rudder(self,msg):
+        self.tack_rudder = msg.data
