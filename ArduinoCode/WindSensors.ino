2db69fb1be7c37dd04ebdff429e474434ea97884
Nanoseb <sebastien.lemaire@ecl13.ec-lyon.fr>
2016-04-30 13:36:38 +0100

    code restructuration for wind sensors

--- ArduinoCode/WindSensors.ino
+++ ArduinoCode/WindSensors.ino
@@ -18,11 +18,22 @@ std_msgs::Float64 apparent_wind_angle;
 ros::Publisher publishApparentWindSpeed("/wind_speed_apparent", &apparent_wind_speed);
 ros::Publisher publishApparentWindAngle("/wind_direction_apparent", &apparent_wind_angle);
 
-float wind_speed = 0;  // Initialise wind speed
-int timestep = 100;   // timestep in [ms] 
+float wind_speed = 0;   // Initialise wind speed
+int timestep = 100;     // timestep in [ms] 
 
-int PIN = 2; // For the wind speed sensor, pin 2 or 3 only
+int PIN = 2;            // For the wind speed sensor, pin 2 or 3 only
 int count = 0;
+
+int   i;                // count number (for loop)
+
+
+// read raw sensor data from the analog PINs
+// Initialisation for the wind directions sensor
+int   Reference[16] = { 422,   487, 516,  603, 707,  780, 818,   891, 915,   958, 965,   980, 994,   1001, 1009, 1016};
+float Direction[16] = {112.5, 157.5, 135, 67.5,  90, 22.5,  45, 202.5, 180, 337.5,   0, 247.5, 225,  292.5,  270,  315};
+int   position=-1,thread=100;
+int   sensorValue1 
+
 
 void arduino_anemometer()
 {
@@ -37,6 +48,7 @@ void setup()
   nh.advertise(publishApparentWindAngle);
   attachInterrupt(PIN , arduino_anemometer, RISING);
 
+  sensorValue1 = analogRead(A0);  // Pin A0 for the wind direction
 }
 
 
@@ -53,13 +65,7 @@ void loop()
   delay(timestep);
   wind_speed = (666.66/timestep) * count;
 
-  // read raw sensor data from the analog PINs
-  int   i; // count number
-  int   sensorValue1 =analogRead(A0);  // Pin A0
-  int   Reference[16] = {  422,   487, 516,  603, 707,  780, 818,   891, 915,   958, 965,   980, 994,   1001, 1009, 1016};
-  float Direction[16] = {112.5, 157.5, 135, 67.5,  90, 22.5,  45, 202.5, 180, 337.5,   0, 247.5, 225,  292.5,  270,  315};
-  int   position=-1,thread=100;
-
+  thread=100;
   // Update apparent wind angle
   for(i=0;i<=15;i++){
         if(abs(Reference[i]-sensorValue1)<thread){
