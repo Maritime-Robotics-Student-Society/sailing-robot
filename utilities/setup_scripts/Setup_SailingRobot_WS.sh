49298ec57be901bade5abf1203e09be077b22fcf
NanaOKA <nana.abankwa@gmail.com>
2016-06-06 18:08:09 +0100

    Add files via upload

---
+++ utilities/setup_scripts/Setup_SailingRobot_WS.sh
@@ -0,0 +1,19 @@
+#!/bin/sh 
+
+cd ~
+
+# Clone from git
+git clone https://github.com/Maritime-Robotics-Student-Society/sailing-robot.git
+
+# Resolve dependencies with rosdep
+cd ~/sailing-robot
+rosdep install --from-paths src --ignore-src --rosdistro indigo -y -r --os=debian:jessie
+
+# Build catkin workspace
+catkin_make
+
+# Source new installation
+source devel/setup.bash
+echo "source devel/setup.bash" >> ~/.bashrc
+
+cd ~
