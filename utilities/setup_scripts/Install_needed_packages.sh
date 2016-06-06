eb0e039d49fe175e2ef1fdab3d9b9359fbeec011
NanaOKA <nana.abankwa@gmail.com>
2016-06-06 18:21:16 +0100

    Update Install_needed_packages.sh

--- utilities/setup_scripts/Install_needed_packages.sh
+++ utilities/setup_scripts/Install_needed_packages.sh
@@ -4,6 +4,9 @@ cd ~
 # Install Latlon and pynmea2 for gps
 yes | sudo pip install Latlon pynmea2
 
+# Needed for serial
+yes | sudo pip install spidev
+
 # Install vim
 sudo apt-get --assume-yes install vim
 cd ~
