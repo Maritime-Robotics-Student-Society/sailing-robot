a03aa6a80176226602eda67a60fea653dd703f9d
Nanoseb <sebastien.lemaire@ecl13.ec-lyon.fr>
2016-07-01 13:15:41 +0100

    change mothod names

--- src/sailing_robot/tests/test_angle_average.py
+++ src/sailing_robot/tests/test_angle_average.py
@@ -3,12 +3,12 @@ import unittest
 from sailing_robot.navigation import angle_average
 
 class AngleAverageTests(unittest.TestCase):
-    def realValues(self):
+    def test_realValues(self):
         self.assertAlmostEqual(angle_average([1,2,3,4,5]), 3)
         self.assertAlmostEqual(angle_average([10,90]), 50)
         self.assertAlmostEqual(angle_average([10,350]), 0)
 
-    def smoketest(self):
+    def test_extreme(self):
         angle_average([])
 
         angle_average([1,1,1])
