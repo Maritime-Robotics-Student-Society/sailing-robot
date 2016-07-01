675d1436b38faa966fb8c256fb51f710e1983cc6
Thomas Kluyver <thomas@kluyver.me.uk>
2016-07-01 23:00:43 +0100

    Offset test by 1

--- src/sailing_robot/tests/test_angle_average.py
+++ src/sailing_robot/tests/test_angle_average.py
@@ -6,7 +6,7 @@ class AngleAverageTests(unittest.TestCase):
     def test_realValues(self):
         self.assertAlmostEqual(angle_average([1,2,3,4,5]), 3)
         self.assertAlmostEqual(angle_average([10,90]), 50)
-        self.assertAlmostEqual(angle_average([10,350]), 0)
+        self.assertAlmostEqual(angle_average([11,351]), 1)
 
     def test_extreme(self):
         angle_average([])
