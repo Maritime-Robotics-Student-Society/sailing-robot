09f4626757c8a82eda96fcf70293ced674a3eb29
Nanoseb <sebastien.lemaire@ecl13.ec-lyon.fr>
2016-09-07 09:46:21 +0100

    fix pushing to pi

--- utilities/push2pi.sh
+++ utilities/push2pi.sh
@@ -5,4 +5,4 @@ git push pi@192.168.42.1:sailing-robot-bare
 
 
 echo pulling from the pi
-ssh pi@192.168.42.1 'git pull bareclone'
+ssh pi@192.168.42.1 'cd ~/sailing-robot; git pull bareclone master'
