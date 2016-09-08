519da0409f20efa8fbb7ae22f5330fc7f57b586a
Nanoseb <sebastien.lemaire@ecl13.ec-lyon.fr>
2016-09-08 11:09:07 +0100

    update area scanning generator

--- utilities/waypoint_generator_area_scanning.py
+++ utilities/waypoint_generator_area_scanning.py
@@ -16,10 +16,11 @@ yaml_data = yaml.load(file(input_file, 'r'), Loader=yaml.Loader)
 output_file = input_file[:-5] + "_gen_area.yaml"
 
 
-wpA = yaml_data['wp/table']['A']
-wpB = yaml_data['wp/table']['B']
-wpC = yaml_data['wp/table']['C']
-wpD = yaml_data['wp/table']['D']
+wpA = yaml_data['wp/table']['A'] # top left (start line)
+wpB = yaml_data['wp/table']['B'] # start line (second point)
+wpC = yaml_data['wp/table']['C'] #finish line (top)
+wpD = yaml_data['wp/table']['D'] # finish line (second point)
+#wpE = yaml_data['wp/table']['E'] # top right corner
 
 
 nav = Navigation()
@@ -27,14 +28,16 @@ wpA_utm = nav.latlon_to_utm(wpA[0], wpA[1])
 wpB_utm = nav.latlon_to_utm(wpB[0], wpB[1])
 wpC_utm = nav.latlon_to_utm(wpC[0], wpC[1])
 wpD_utm = nav.latlon_to_utm(wpD[0], wpD[1])
+#wpE_utm = nav.latlon_to_utm(wpE[0], wpE[1])
 
 
 # Unit vector AB
-vAB = np.array([wpB_utm[0] - wpA_utm[0], wpB_utm[1] - wpA_utm[1]])/4.0
+vAB = np.array([wpB_utm[0] - wpA_utm[0], wpB_utm[1] - wpA_utm[1]])/3.0
 
 # Unit vector orth to AB
 vAB_orth = np.array([-vAB[1], vAB[0]])
-
+#vAE = np.array([wpE_utm[0] - wpA_utm[0], wpE_utm[1] - wpA_utm[1]])/6.0
+#vAB_orth = vAE
 
 #### Start line
 wp_start = [wpA_utm +vAB/2 - vAB_orth/2]
@@ -43,27 +46,27 @@ wp_start = [wpA_utm +vAB/2 - vAB_orth/2]
 #### TOP 8x4 part
 wp_list_top = [wpA_utm - vAB/2 + vAB_orth/2]
 dir = 1
-for wp_idx_vert in range(4):
+for wp_idx_vert in range(3):
     wp_list_top.append(wp_list_top[-1] + vAB)
-    for wp_idx_hor in range(7):
+    for wp_idx_hor in range(5):
         wp_list_top.append(wp_list_top[-1] + dir*vAB_orth)
     dir = -dir
 wp_list_top.pop(0) # to remove the first wp (only there because it was easier to loop like that...)
 
 
 #### BOTTOM 4x4 part
-wp_list_bot = [wpA_utm + vAB*3.5 + vAB_orth*4.5]
-dir = 1
-for wp_idx_vert in range(4):
+wp_list_bot = [wpA_utm + vAB*2.5 + vAB_orth*5.5]
+dir = -1
+for wp_idx_vert in range(3):
     wp_list_bot.append(wp_list_bot[-1] + vAB)
-    for wp_idx_hor in range(3):
+    for wp_idx_hor in range(2):
         wp_list_bot.append(wp_list_bot[-1] + dir*vAB_orth)
     dir = -dir
 wp_list_bot.pop(0) # to remove the first wp (only there because it was easier to loop like that...)
 
 
 #### Finish line
-wp_finish = [wpA_utm + vAB*7 + vAB_orth/2, wpA_utm + vAB*7 - vAB_orth/2]
+wp_finish = [wpA_utm + vAB*5 + vAB_orth/2, wpA_utm + vAB*5 - vAB_orth/2]
 # wp_finish = [wpC_utm + vAB + vAB_orth/2, wpC_utm + vAB - vAB_orth/2] # uncomment if wpC is correctly set
 
 
