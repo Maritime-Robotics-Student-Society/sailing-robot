743f66c207cd598f359b1beaf6cade679780861e
Thomas Kluyver <thomas@kluyver.me.uk>
2016-09-15 19:56:59 +0100

    Separate sail sheet demand from servo control
    Closes gh-138

--- src/sailing_robot/tests/test_sail_table.py
+++ src/sailing_robot/tests/test_sail_table.py
@@ -1,15 +1,25 @@
 from nose.tools import assert_equal, assert_almost_equal
-from sailing_robot.sail_table import SailTable
+from sailing_robot.sail_table import SailTable, SailData
+
+SAMPLE_SAIL_TABLE = {
+    '0': 0,
+    '30': 0,
+    '90': 0.5,
+    '180': 0.9,
+}
 
 def test_sail_table():
-    st = SailTable({
-        '0': 0,
-        '30': 0,
-        '90': 0.5,
-        '180': 0.9,
-    })
+    st = SailTable(SAMPLE_SAIL_TABLE)
     assert_almost_equal(st.interpolate_sail_setting(20), 0)
     assert_almost_equal(st.interpolate_sail_setting(60), 0.25)
     assert_almost_equal(st.interpolate_sail_setting(90), 0.5)
     assert_almost_equal(st.interpolate_sail_setting(135), 0.7)
     assert_almost_equal(st.interpolate_sail_setting(190), 0.9)
+
+def test_sail_data():
+    st = SailTable(SAMPLE_SAIL_TABLE)
+    sd = SailData(st, sheet_out_to_jibe=True, jibe_sheet_setting=0.75)
+    sd.wind_direction_apparent = 90
+    assert_almost_equal(sd.calculate_sheet_setting(), 0.5)
+    sd.sailing_state = 'jibe_to_stbd_tack'
+    assert_almost_equal(sd.calculate_sheet_setting(), 0.75)
