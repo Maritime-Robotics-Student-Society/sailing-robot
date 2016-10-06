fb472727c61262570d019e9ec9857f877cf00b3e
Thomas Kluyver <thomas@kluyver.me.uk>
2016-10-06 18:51:15 +0100

    Playing with reading PWM from RC

---
+++ utilities/read_pwm.py
@@ -0,0 +1,25 @@
+import pigpio
+import time
+
+GPIO = 22
+
+last_tick = 0
+last_interval = 0
+
+def tick(gpio, level, tick):
+    global last_tick
+    last_tick = tick
+
+def tock(gpio, level, tick):
+    global last_interval
+    last_interval = tick - last_tick
+
+pi = pigpio.pi()
+pi.set_mode(GPIO, pigpio.INPUT)
+
+pi.callback(GPIO, pigpio.RISING_EDGE, tick)
+pi.callback(GPIO, pigpio.FALLING_EDGE, tock)
+
+while True:
+    time.sleep(1)
+    print(last_interval)
