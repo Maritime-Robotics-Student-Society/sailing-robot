11bde376119f49921ed7ac4fcea98986b888fa36
Nanoseb <sebastien.lemaire@ecl13.ec-lyon.fr>
2016-12-24 00:09:15 +0100

    use hsv for camera calibration

--- utilities/camera_detection_calibration.py
+++ utilities/camera_detection_calibration.py
@@ -2,6 +2,7 @@
 # READY FOR MIT
 
 import rospy
+import sys
 import math
 import time
 
@@ -10,46 +11,57 @@ import cv2
 
 Lower = np.array( rospy.get_param('camera_detection/Lower_color'))
 Upper = np.array( rospy.get_param('camera_detection/Upper_color'))
+Lower_hsv1 = np.array( rospy.get_param('camera_detection/Lower_color_hsv1'))
+Upper_hsv1 = np.array( rospy.get_param('camera_detection/Upper_color_hsv1'))
+Lower_hsv2 = np.array( rospy.get_param('camera_detection/Lower_color_hsv2'))
+Upper_hsv2 = np.array( rospy.get_param('camera_detection/Upper_color_hsv2'))
 threshold = rospy.get_param('camera_detection/threshold')
 
 
-camera = cv2.VideoCapture(1) 
-(bool, image) = camera.read()
+#camera = cv2.VideoCapture(0) 
+#(bool, image) = camera.read()
+image = cv2.imread(sys.argv[1])
 image_size = image.shape[0]*image.shape[1]
 
 print 'Calibration procedure for the camera'
 print 'use up/j or down/k to change the threshold (=% of the image that is expected to be in the range of colors'
 
-
-while not rospy.is_shutdown():
+files = sys.argv[1:]
+for file in files:
     try: 
         print ''
         print '--------------------------------------'
+        print file
         print ''
-        (bool, image) = camera.read()
+        image = cv2.imread(file)
         if bool:
-            cv2.imwrite('image.png',image)
+            #cv2.imwrite('image.png',image)
             mask = cv2.inRange(image, Lower, Upper)
-
-            percent_detect = 1.0*cv2.countNonZero(mask)  / image_size # percentage of the image that contains the expected colors
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+            mask_hsv1 = cv2.inRange(hsv, Lower_hsv1, Upper_hsv1)
+            mask_hsv2 = cv2.inRange(hsv, Lower_hsv2, Upper_hsv2)
+             
+            percent_detect1 = 1.0*cv2.countNonZero(mask_hsv1)  / image_size # percentage of the image that contains the expected colors
+            print percent_detect1 
+            percent_detect2 = 1.0*cv2.countNonZero(mask_hsv2)  / image_size # percentage of the image that contains the expected colors
+            print percent_detect2 
             
-            if percent_detect >= threshold:
+            print percent_detect2 + percent_detect1 
+            if percent_detect1+percent_detect2  >= threshold: 
                 print 'detected'
             else:
                 print 'nothing detected'
             
             print 'threshold: ' + str(threshold)
 
-            masked_image = cv2.bitwise_and(image, image, mask=mask)
+            masked_image = cv2.bitwise_and(image, image, mask=mask_hsv1 )
             cv2.imshow('image', masked_image)
             key = cv2.waitKey()
 
-#            if key == 65362 or key == ord('k'): # up arrow key of k
-#                threshold = threshold + 0.01
-#            elif key == 65364 or key == ord('j'):# down arrow key of j
-#                threshold = threshold - 0.01
-
-
+            if key == 65362 or key == ord('k'): # up arrow key of k
+                threshold = threshold + 0.01
+            elif key == 65364 or key == ord('j'):# down arrow key of j
+                threshold = threshold - 0.01
 
 
     except KeyboardInterrupt:
