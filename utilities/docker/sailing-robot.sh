7b5769343715a476f2920d818a829b52cfd6d382
Thomas Kluyver <thomas@kluyver.me.uk>
2017-01-26 19:30:27 +0000

    Add utility scripts to start and connect to docker container

---
+++ utilities/docker/sailing-robot.sh
@@ -0,0 +1,14 @@
+# Start the ROS docker container, with this repository mounted inside it as
+# $HOME/sailing-robot
+
+# This script lives in utilities/docker/ 
+# The sailing-robot repo is two levels up.
+this_file=$(readlink -f "$0")
+docker=$(dirname "$this_file")
+utils=$(dirname "$docker")
+repo=$(dirname "$utils")
+
+echo "Starting docker container with $repo mounted at ~/sailing-robot"
+
+docker run --name sailing-robot -h sailing-robot -v "$repo:/home/pi/sailing-robot"\
+      --rm -it jamak9/sailing-robot:indigo
---
+++ utilities/docker/sr-bash.sh
@@ -0,0 +1,3 @@
+# Open another terminal to an existing sailing-robot docker container.
+# This works when you have run sailing-robot.sh to start the docker container.
+docker exec -it sailing-robot bash
