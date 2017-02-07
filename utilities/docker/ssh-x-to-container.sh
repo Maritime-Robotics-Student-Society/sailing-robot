5f3d4bf3baff4a0918f9d49286cdb63655d1c8ce
Thomas Kluyver <thomas@kluyver.me.uk>
2017-02-07 19:46:08 +0000

    Fix name in script comments

--- utilities/docker/ssh-x-to-container.sh
+++ utilities/docker/ssh-x-to-container.sh
@@ -1,4 +1,4 @@
 # SSH to the running docker container with X forwarding.
-# This works when you have run sailing-robot.sh to start the docker container,
+# This works when you have run start-container.sh to start the docker container,
 # and allows you to run graphical programs such as rviz. Probably Linux-only.
 ssh 172.17.0.2 -l pi -X
