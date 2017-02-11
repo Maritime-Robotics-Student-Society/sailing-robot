fc2f28fba16b5064802178aff1a8797a9552aef5
nanoseb <sebastien.lemaire@ecl13.ec-lyon.fr>
2017-02-11 18:06:05 +0100

    add documentation to scripts

--- utilities/extravaganza_setup_script.sh
+++ utilities/extravaganza_setup_script.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# This script is useful when using Tabarly as a ros server
+# It will create a wifi network (called Tabarly), and run roscore
+# People then connect to it via ssh: ssh -Y soton-pirate@192.168.12.
+# And can use their own ros environement
+# 
+# Once done, simply hit ctrl+c to kill ros and stop the wifi network
+
 
 trap ctrl_c INT
 function ctrl_c() {
@@ -8,9 +15,10 @@ function ctrl_c() {
 	sleep 1
 }
 
+
+# Detection of the wifi interface
 wifi_interface=$(ifconfig | grep wlan | grep -v wlan0 | awk '{print $1}' | tail -n 1)
 
-# Creation of the access point
 
 echo "
 Tell people to connect to Tabarly wifi (the small wifi adapter is needed)
@@ -21,6 +29,7 @@ Then they have to ssh to tabarly via:
 ssh -Y soton-pirate@192.168.12.1
 Password: pirates!"
 
+# Creation of the access point
 sudo create_ap --daemon --no-virt $wifi_interface wlan0 Tabarly 'pirates!' > /dev/null
 
 echo 'running roscore'
