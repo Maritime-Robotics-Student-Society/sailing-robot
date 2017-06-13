8198142a6f7c2c1966332746ceb4696697049216
Thomas Kluyver <thomas@kluyver.me.uk>
2017-06-13 19:01:09 +0100

    Pick serial port name for raspi 2 and 3

--- src/sailing_robot/src/sailing_robot/gps_utils.py
+++ src/sailing_robot/src/sailing_robot/gps_utils.py
@@ -1,3 +1,4 @@
+import os
 import struct
 import sys
 
@@ -26,3 +27,10 @@ def ubx_checksum(msg):
         a = (a + c) & 0xff
         b = (b + a) & 0xff
     return struct.pack('BB', a, b)
+
+def get_port():
+    """The serial port for the GPS has different names on raspi 2 and 3.
+    """
+    if os.path.exists('/its_raspi3'):
+        return "/dev/serial0"  # Raspi 3
+    return "/dev/ttyAMA0"  # Raspi 2
