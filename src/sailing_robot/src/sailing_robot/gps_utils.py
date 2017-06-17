77ff9f28b2e3c19bbd93fb5fbe2676a5d0130c64
Thomas Kluyver <thomas@kluyver.me.uk>
2017-06-17 17:03:25 +0100

    Implement combined parser for Ublox and NMEA messages

--- src/sailing_robot/src/sailing_robot/gps_utils.py
+++ src/sailing_robot/src/sailing_robot/gps_utils.py
@@ -1,6 +1,7 @@
 import os
 import struct
 import sys
+from pynmea2 import NMEASentence
 
 if sys.version_info[0] >= 3:
     def iter_ints(b):
@@ -34,3 +35,61 @@ def get_port():
     if os.path.exists('/its_raspi3'):
         return "/dev/serial0"  # Raspi 3
     return "/dev/ttyAMA0"  # Raspi 2
+
+class UbxNmeaParser(object):
+    def __init__(self):
+        self.buf = b''
+
+    def feed(self, data):
+        self.buf += data
+
+    def _take_chunk(self, n):
+        c, self.buf = self.buf[:n], self.buf[n:]
+        return c
+
+    def _take_nmea(self):
+        crlf_ix = self.buf.find(b'\r\n')
+        if crlf_ix == -1:
+            return None   # Don't have a complete message yet
+        data = self._take_chunk(crlf_ix + 2)
+        return NMEASentence.parse(data)
+
+    def _take_ubx(self):
+        if len(self.buf) < 6:
+            return None    # Don't have the length yet
+        payload_length = struct.unpack('<H', self.buf[4:6])
+        msg_length = payload_length + 8
+        if len(self.buf) >= msg_length:
+            return self._take_chunk(msg_length)
+
+    def _next_msg(self):
+        nmea_start = self.buf.find(b'$')
+        ubx_start = self.buf.find(b'\xb5b')
+        if nmea_start == 0:
+            return self._take_nmea()
+        elif ubx_start == 0:
+            pass
+        elif (nmea_start > 0) and (ubx_start > 0):
+            return self._take_chunk(min(nmea_start, ubx_start))
+        elif nmea_start > 0:
+            return self._take_chunk(nmea_start)
+        elif ubx_start > 0:
+            return self._take_chunk(ubx_start)
+
+    def get_msgs(self):
+        return iter(self._next_msg, None)
+
+def test_stream_parser(filename):
+    import random
+    parser = UbxNmeaParser()
+    with open(filename, 'rb') as f:
+        while True:
+            b = f.read(random.randint(1, 100))
+            if b == b'':
+                return
+            parser.feed(b)
+            for msg in parser.get_msgs():
+                print(repr(msg))
+
+if __name__ == '__main__':
+    test_stream_parser(sys.argv[1])
