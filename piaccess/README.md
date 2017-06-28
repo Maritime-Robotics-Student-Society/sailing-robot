c74726c347b3888e16afca4f5d16af6ce79734e0
Thomas Kluyver <thomas@kluyver.me.uk>
2017-06-28 21:51:21 +0100

    Add folder of scripts for talking to raspi

---
+++ piaccess/README.md
@@ -0,0 +1,13 @@
+This folder contains scripts for communicating with the raspis we use.
+
+First, source one of the pi identity files, which set $SAIL_PI_IP:
+
+    source piaccess/magellan
+    # OR
+    source piaccess/car
+
+Then run:
+
+- `piaccess/ssh` : SSH to the raspi
+- `piaccess/time2pi.sh` : Set the raspi's clock from your computer
+- `piaccess/push2pi.sh` : Push this git repository to the raspi
---
+++ piaccess/time2pi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Send the time from this computer to the raspi
+
+# This is not suitable for setting a precise time: it makes no effort to account
+# for the delay between reading the local clock and setting it on the raspi.
+# But when the raspi is powered on, its clock can easily be days out, so even
+# setting an approximate time is important.
+
+# See also utilities/setclock, which sets the clock from a GPS signal
+
+set -e
+
+echo "Sending time to the pi at ${SAIL_PI_IP:=192.168.42.1}..."
+TIMESTAMP=$(date --utc +%Y-%m-%dT%H:%M:%S)
+
+set -x
+ssh pi@$SAIL_PI_IP "sudo date --utc +%Y-%m-%dT%H:%M:%S -s $TIMESTAMP"
