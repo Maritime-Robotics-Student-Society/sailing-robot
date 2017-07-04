c59acb8e30f6450e7027d8fabd435e6b7bed89e2
Thomas Kluyver <thomas@kluyver.me.uk>
2017-07-04 20:11:25 +0100

    Script for monitoring IMU separate from calibration

---
+++ utilities/calibration_scripts/monitor_imu.py
@@ -0,0 +1,40 @@
+import curses
+import os
+import sys
+import time
+
+my_dir = os.path.dirname(__file__)
+robot_src_dir = os.path.abspath(os.path.join(my_dir, '../../src/sailing_robot/src'))
+sys.path.append(robot_src_dir)
+
+from sailing_robot.imu_utils import ImuReader
+from curses_imu import IMUDisplay, pitch_roll
+
+IMU_BUS = 1
+LGD = 0x6b #Device I2C slave address
+LSM = 0x1d #Device I2C slave address
+
+imu = ImuReader(IMU_BUS, LSM, LGD)
+imu.check_status()
+imu.configure_for_reading()
+
+
+def monitor(stdscr):
+    display = IMUDisplay(stdscr)
+    stdscr.addstr(curses.LINES-1, 0, 'Press Ctrl-C to quit')
+    
+    while True:
+        time.sleep(0.1)
+        
+        magx, magy, magz = imu.read_mag_field()
+        accx, accy, accz = imu.read_acceleration()
+
+        display.update_mag(magx, magy, magz)
+        display.update_acc(accx, accy, accz)
+        display.update_pitch_roll(*pitch_roll(accx, accy, accz))
+        stdscr.refresh()
+
+try:
+    curses.wrapper(monitor)
+except KeyboardInterrupt:
+    print('Stopped')
