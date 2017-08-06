3564a5f288f46b5f579a90ce29a6a8225a8768ae
Thomas Kluyver <thomas@kluyver.me.uk>
2017-08-06 11:41:03 +0100

    Add script to git pull from the pi

---
+++ piaccess/gitpull.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+echo "Pushing to bareclone on the pi at ${SAIL_PI_IP:=192.168.42.1}..."
+ssh pi@$SAIL_PI_IP 'cd ~/sailing-robot; git push bareclone master'
+
+echo "Pulling from the pi..."
+git pull pi@$SAIL_PI_IP:sailing-robot-bare master
--- piaccess/push2pi.sh
+++ piaccess/push2pi.sh
@@ -5,5 +5,5 @@ echo "Pushing to the pi at ${SAIL_PI_IP:=192.168.42.1}..."
 git push pi@$SAIL_PI_IP:sailing-robot-bare
 
 
-echo "Pulling from the pi..."
+echo "Pulling from bareclone on the pi..."
 ssh pi@$SAIL_PI_IP 'cd ~/sailing-robot; git pull bareclone master'
