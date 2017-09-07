bf8e9c147ffeed9b3d4670ac07c6f6f2950086c8
Thomas Kluyver <thomas@kluyver.me.uk>
2017-09-07 13:39:50 +0200

    Fix pullfrompi script

--- piaccess/pullfrompi.sh
+++ piaccess/pullfrompi.sh
@@ -2,8 +2,7 @@
 set -e
 
 echo "On ${SAIL_PI_IP:=192.168.12.1}, pushing to bareclone..."
-git pull pi@$SAIL_PI_IP:sailing-robot-bare master
-
+ssh pi@$SAIL_PI_IP 'cd ~/sailing-robot; git push bareclone master'
 
 echo "Pulling from the pi..."
-ssh pi@$SAIL_PI_IP 'cd ~/sailing-robot; git pull bareclone master'
+git pull pi@$SAIL_PI_IP:sailing-robot-bare master
