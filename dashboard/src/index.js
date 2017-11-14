2c1dadb6c7151872721d043e0f189617c9d4406e
Zhong-yuen Lee <zl21g13@soton.ac.uk>
2017-11-14 19:20:59 +0000

    Testing RoslibJS connections
    This is a simple test to try out connecting to ROS via RoslibJS.
    Nothing else.

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,6 +1,22 @@
 /////////////////////////////////////////
 //          Web Socket Stuff           //
 /////////////////////////////////////////
+const ros = new ROSLIB.Ros({
+  url: 'ws://192.168.12.1:8448/updates'
+});
+
+ros.on('connection', function() {
+  console.log('Connected to websocket server.');
+});
+
+ros.on('error', function (error) {
+  console.log('Error connecting to websocket server:', error);
+});
+
+ros.on('close', function () {
+  console.log('Connection to websocket server closed.');
+});
+
 const ws = new WebSocket(`ws://192.168.12.1:8448/updates`);
 ws.onopen = function (ws, event) {
   connectionOverlays.isDisconnected = false;
