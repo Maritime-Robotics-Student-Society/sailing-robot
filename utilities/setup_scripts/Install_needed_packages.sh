16d396b5d19fb53d046ed970986ddd49182a98be
Thomas Kluyver <thomas@kluyver.me.uk>
2018-03-06 21:28:23 +0000

    Messages from setup script

--- utilities/setup_scripts/Install_needed_packages.sh
+++ utilities/setup_scripts/Install_needed_packages.sh
@@ -1,8 +1,14 @@
 #!/bin/sh 
 set -e
 
-# Install Geos headers (for shapely), and scipy (for compass calibration), install pip too
-sudo apt-get --assume-yes install libgeos-dev python-scipy python-pip
+# Install:
+# Geos headers (for shapely)
+# scipy (for compass calibration)
+# pip (Python packages)
+# vim
+# bc (calculator)
+echo "Installing apt packages..."
+sudo apt-get --assume-yes install libgeos-dev python-scipy python-pip vim bc
 
 # Install:
 # Latlon, shapely, pyproj (navigation)
@@ -10,16 +16,19 @@ sudo apt-get --assume-yes install libgeos-dev python-scipy python-pip
 # spidev (Needed for serial)
 # tornado (web server for HTML dashboard)
 # ina219 library (voltmeter/currentmeter)
+echo "Installing Python packages..."
+yes | sudo pip install --upgrade pip
 yes | sudo pip install Latlon shapely pyproj pynmea2 spidev tornado pi_ina219
 
-# Install vim & bc (calculator)
-sudo apt-get --assume-yes install vim bc
-
 # Set time zone to england (it is the same for portugal)
+echo "Setting timezone..."
 sudo timedatectl set-timezone Europe/London
 
 # pigpio - for talking to GPIO pins (including daemon service)
+echo "Installing pigpio from apt..."
 sudo apt-get --assume-yes install pigpio python-pigpio
+echo "Installing and starting pigpio service..."
 sudo cp pigpio.service /lib/systemd/system
 sudo systemctl enable pigpio
 sudo systemctl start pigpio
+echo "Done."
