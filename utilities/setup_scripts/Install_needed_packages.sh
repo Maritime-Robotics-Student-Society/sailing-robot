522520cc127eb6dd07cc5f8cebd1f43d615d1459
Thomas Kluyver <thomas@kluyver.me.uk>
2018-03-06 21:05:09 +0000

    Consolidate installation commands in setup script

--- utilities/setup_scripts/Install_needed_packages.sh
+++ utilities/setup_scripts/Install_needed_packages.sh
@@ -1,28 +1,20 @@
 #!/bin/sh 
+set -e
 cd ~
 
 # Install Geos headers (for shapely), and scipy (for compass calibration), install pip too
 sudo apt-get --assume-yes install libgeos-dev python-scipy python-pip
 
-# Install Latlon, shapely (navigation), pynmea2 (reading GPS)
-yes | sudo pip install Latlon shapely pynmea2
+# Install:
+# Latlon, shapely, pyproj (navigation)
+# pynmea2 (reading GPS)
+# spidev (Needed for serial)
+# tornado (web server for HTML dashboard)
+# ina219 library (voltmeter/currentmeter)
+yes | sudo pip install Latlon shapely pyproj pynmea2 spidev tornado pi_ina219
 
-# Needed for serial
-yes | sudo pip install spidev
-
-
-# Install pyproj (cartographic projections)
-yes | sudo pip install pyproj
-
-# Install vim
-sudo apt-get --assume-yes install vim
-
-# Install bc
-sudo apt-get --assume-yes install bc
-
-
-# Needed for dashboard
-yes | sudo pip install tornado
+# Install vim & bc (calculator)
+sudo apt-get --assume-yes install vim bc
 
 # Set time zone to england (it is the same for portugal)
 sudo timedatectl set-timezone Europe/London
@@ -32,9 +24,5 @@ sudo apt-get --assume-yes install pigpio python-pigpio
 sudo cp pigpio.service /lib/systemd/system
 sudo systemctl enable pigpio
 sudo systemctl start pigpio
-
-# Install ina219 library (voltmeter/currentmeter)
-sudo pip install pi_ina219
-
 
 cd ~
