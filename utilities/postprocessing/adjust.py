a34e807e5103a647ab3f38caf5057a2cb61b2c8a
Tabarly <sotonsailrobot@gmail.com>
2018-09-01 21:02:19 +0100

    update adjust script

--- utilities/postprocessing/adjust.py
+++ utilities/postprocessing/adjust.py
@@ -7,17 +7,22 @@ from datetime import datetime, timedelta
 from pathlib import Path
 import re
 
-CORRECTION = timedelta(10, 60496)
-
+# CORRECTION = timedelta(10, 60496)
+CORRECTION = timedelta(seconds=(1535393272-1529482132)) # epoch from good - raspi
 TIMESTAMP_RE = r'(\d{4})-(\d{2})-(\d{2})-?T?(\d{2})[-.](\d{2})[-.](\d{2})'
 
-for path in sorted(Path.cwd().glob('*_2017-04-1*')):
+#for path in sorted(Path.cwd().glob('*_2018-06-20-*')):
+for path in sorted(Path.cwd().glob('*_triangle*')):
     print('--')
     print(path.name)
     m = re.search(TIMESTAMP_RE, path.name)
     old_ts = datetime(*[int(g) for g in m.groups()])
     new_ts = old_ts + CORRECTION
-    new_name = path.name[:m.start()] + new_ts.strftime("%Y-%m-%dT%H.%M.%S") \
+    if "T" in path.name:
+	new_name = path.name[:m.start()] + new_ts.strftime("%Y-%m-%dT%H.%M.%S") \
+                + path.name[m.end():]
+    else:
+        new_name = path.name[:m.start()] + new_ts.strftime("%Y-%m-%d-%H-%M-%S") \
                 + path.name[m.end():]
     print(new_name)
     new_file = path.parent / new_name
